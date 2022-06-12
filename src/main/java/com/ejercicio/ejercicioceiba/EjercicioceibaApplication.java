1cc093fb636ac115727df0a86bc385a6d5ef415a
AimerSanchez <aimersc09@hotmail.com>
2022-06-12 13:35:17 -0500

    GRAL: Se realiza el primer commit para la base del proyecto de ceiba

---
+++ src/main/java/com/ejercicio/ejercicioceiba/EjercicioceibaApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio.ejercicioceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioceibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioceibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejercicio/ejercicioceiba/EjercicioceibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.ejercicioceiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioceibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
